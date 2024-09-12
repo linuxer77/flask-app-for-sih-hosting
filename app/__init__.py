3d3f43cab3dd987299b5bd7ef3da1e1bb47e862e
linuxer77 <harshitg187@gmail.com>
2024-09-12 10:34:00 +0530

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
-
+CORS(app, resources={r"/*": {"origins": "https://tourism-and-travel-sih.vercel.app"}})
 from app import routes
 if __name__ == '__main__':
     app.run(debug=True)
